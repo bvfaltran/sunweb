d16ad5d3205cfcb2b0cbecc1487b096ec9884c7b
brais <brais.vazquezfarre@altran.com>
2016-06-10 13:30:19 +0200

    fonts folders moved to assets and exportation script added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 var input = './stylesheets/**/*.scss';
+
+var pacificoOrigin = ['./assets/pacifico/*.eot','./assets/pacifico/*.svg','./assets/pacifico/*.ttf','./assets/pacifico/*.woff','./assets/pacifico/*.woff2'];
+var forsdOrigin = ['./assets/forsd-folly/*.eot','./assets/forsd-folly/*.svg','./assets/forsd-folly/*.ttf','./assets/forsd-folly/*.woff','./assets/forsd-folly/*.woff2'];
+var sansaProOrigin = ['./assets/sansa-pro/*.eot','./assets/sansa-pro/*.svg','./assets/sansa-pro/*.ttf','./assets/sansa-pro/*.woff','./assets/sansa-pro/*.woff2'];
+var sansaProBoldOrigin = ['./assets/sansa-pro-bold/*.eot','./assets/sansa-pro-bold/*.svg','./assets/sansa-pro-bold/*.ttf','./assets/sansa-pro-bold/*.woff','./assets/sansa-pro-bold/*.woff2'];
+var iconsOrigin = ['./assets/sunweb-icons/*.eot','./assets/sunweb-icons/*.svg','./assets/sunweb-icons/*.ttf','./assets/sunweb-icons/*.woff','./assets/sunweb-icons/*.woff2'];
+var ywftOrigin = ['./assets/ywft-signature/*.eot','./assets/ywft-signature/*.svg','./assets/ywft-signature/*.ttf','./assets/ywft-signature/*.woff','./assets/ywft-signature/*.woff2'];
+
 var output = './public/css';
+var fontsTarget = './public/fonts';
 
 gulp.task('default', function () {
   return gulp
@@ -11,8 +20,42 @@ gulp.task('default', function () {
     // Run Sass on those files
     .pipe(sass())
     // Write the resulting CSS in the output folder
-    .pipe(gulp.dest(output));
+    .pipe(gulp.dest(fontsTarget));
 });
+
+gulp.task('bundleFonts1', function () {
+  return gulp
+    .src(pacificoOrigin)
+    .pipe(gulp.dest(fontsTarget));
+});
+gulp.task('bundleFonts2', function () {
+  return gulp
+    .src(forsdOrigin)
+    .pipe(gulp.dest(fontsTarget))
+});
+gulp.task('bundleFonts3', function () {
+  return gulp
+    .src(sansaProOrigin)
+    .pipe(gulp.dest(fontsTarget))
+});
+gulp.task('bundleFonts4', function () {
+  return gulp
+    .src(sansaProBoldOrigin)
+    .pipe(gulp.dest(fontsTarget))
+});
+gulp.task('bundleFonts5', function () {
+  return gulp
+    .src(iconsOrigin)
+    .pipe(gulp.dest(fontsTarget))
+});
+gulp.task('bundleFonts6', function () {
+  return gulp
+    .src(ywftOrigin)
+    .pipe(gulp.dest(fontsTarget))
+});
+
+gulp.task('bundleFonts', ['bundleFonts1', 'bundleFonts2', 'bundleFonts3', 'bundleFonts4', 'bundleFonts5', 'bundleFonts6']);
+
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
